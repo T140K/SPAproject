29636a1061ff89f01cff69e80e2b7777768d9eac
Darek <darek1430143@gmail.com>
2023-10-25 18:47:08 +0200

    init

---
+++ SPAproject/Models/ApplicationUser.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace SPAproject.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string? Nickname { get; set; }
+
+    }
+}
---
+++ SPAproject/Models/GameModel.cs
@@ -0,0 +1,11 @@
+﻿namespace SPAproject.Models
+{
+    public class GameModel
+    {
+        public int Id { get; set; }
+        public string GameId { get; set; }
+        public string User { get; set; }
+        public int Answer { get; set; }
+        public int GuessAmount { get; set; }
+    }
+}
